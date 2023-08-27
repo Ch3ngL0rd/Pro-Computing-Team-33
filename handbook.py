6334fb2b099a4d9223f20ac4ff5a3cf42a3f4846
Harris <harris.borman@outlook.com>
2023-08-27 19:38:04 +0800

    Merged files & added comments to select_all_rules

--- handbook.py
+++ handbook.py
@@ -1,60 +1,97 @@
 import sqlite3
-import Dbquery
 
 # Connect to an in-memory SQLite database
-conn = sqlite3.connect(':memory:')
+def create_db():
+    conn = sqlite3.connect(':memory:')
+    cursor = conn.cursor()
+
+    # Create Tables
+    cursor.execute('''
+    CREATE TABLE Units (
+        unit_code TEXT PRIMARY KEY,
+        credit_pts INTEGER
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE Groups (
+        group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE RuleUnits (
+        unit_code TEXT,
+        rule_id INTEGER,
+        FOREIGN KEY(unit_code) REFERENCES Units(unit_code),
+        FOREIGN KEY(rule_id) REFERENCES Rule(rule_id),
+        PRIMARY KEY(unit_code, rule_id)
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE Rules (
+        rule_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        value INTEGER
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE Major (
+        major_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        year INTEGER
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE MajorRules (
+        major_id INTEGER,
+        rule_id INTEGER,
+        FOREIGN KEY(major_id) REFERENCES Major(major_id),
+        FOREIGN KEY(rule_id) REFERENCES Rules(rule_id),
+        PRIMARY KEY(major_id, rule_id)
+    );
+    ''')
+
+    return conn
+
+def select_all_rules(conn, major, yr):
+
+    #Gets cursor for conn
+    cursor = conn.cursor()
+    
+    # Query to retrieve the rule values and lists of units for each rule from the db according to provided major & year
+    query = """
+        SELECT r.VALUE, GROUP_CONCAT(u.UNIT_CODE)
+        FROM Rules r
+        INNER JOIN MajorRules rm ON r.rule_id = rm.rule_id
+        INNER JOIN RuleUnits ur ON r.rule_id = ur.rule_id
+        INNER JOIN Units u ON ur.unit_code = u.unit_code
+        INNER JOIN Major m ON rm.major_id = m.major_ID
+        WHERE m.name = ? AND m.year = ?
+        GROUP BY r.rule_id
+    """
+
+    # Execute the query with parameters
+    cursor.execute(query, (major, yr))
+
+    # Fetch all the results
+    results = cursor.fetchall()
+    # Process the results and prepare the output
+    output = []
+    for row in results:
+        #Extract values from row
+        rule_value, unit_codes = row
+        unit_codes_list = unit_codes.split(',')  # Split the comma-separated unit codes
+        output.append((rule_value, unit_codes_list)) #Append to output list
+
+    return output
+
+
+conn = create_db()
 cursor = conn.cursor()
-
-# Create Tables
-cursor.execute('''
-CREATE TABLE Units (
-    unit_code TEXT PRIMARY KEY,
-    credit_pts INTEGER
-);
-''')
-
-cursor.execute('''
-CREATE TABLE Groups (
-    group_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-);
-''')
-
-cursor.execute('''
-CREATE TABLE RuleUnits (
-    unit_code TEXT,
-    rule_id INTEGER,
-    FOREIGN KEY(unit_code) REFERENCES Units(unit_code),
-    FOREIGN KEY(rule_id) REFERENCES Rule(rule_id),
-    PRIMARY KEY(unit_code, rule_id)
-);
-''')
-
-cursor.execute('''
-CREATE TABLE Rules (
-    rule_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    value INTEGER
-);
-''')
-
-cursor.execute('''
-CREATE TABLE Major (
-    major_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    year INTEGER
-);
-''')
-
-cursor.execute('''
-CREATE TABLE MajorRules (
-    major_id INTEGER,
-    rule_id INTEGER,
-    FOREIGN KEY(major_id) REFERENCES Major(major_id),
-    FOREIGN KEY(rule_id) REFERENCES Rules(rule_id),
-    PRIMARY KEY(major_id, rule_id)
-);
-''')
-
 # Insert Sample Data
 cursor.execute("INSERT INTO Units(unit_code, credit_pts) VALUES('CITS2401', 6);")
 cursor.execute("INSERT INTO Units(unit_code, credit_pts) VALUES('ENSC1003', 6);")
@@ -72,30 +109,8 @@ cursor.execute("INSERT INTO MajorRules(major_id, rule_id) VALUES(1, 2);")
 # Commit the changes
 conn.commit()
 
-# Prints the database
-cursor.execute("SELECT * FROM Units;")
-print("Units")
-print(cursor.fetchall())
-cursor.execute("SELECT * FROM Groups;")
-print("Groups")
-print(cursor.fetchall())
-cursor.execute("SELECT * FROM RuleUnits;")
-print("GroupUnits")
-print(cursor.fetchall())
-cursor.execute("SELECT * FROM Rules;")
-print("Rules")
-print(cursor.fetchall())
-cursor.execute("SELECT * FROM Major;")
-print("Major")
-print(cursor.fetchall())
-cursor.execute("SELECT * FROM MajorRules;")
-print("MajorRules")
-print(cursor.fetchall())
-
-
-connection = Dbquery.create_connection(':memory:')
-
-rules = Dbquery.select_all_rules(conn, 'Computer Science', 2023)
+#Test getting the rules for sample data
+rules = select_all_rules(conn, 'Computer Science', 2023)
 
 
 print(rules)
