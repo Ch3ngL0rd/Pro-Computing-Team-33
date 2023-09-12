cc024b9295b5e1bdf8da8fe7e5f76bdf60fcf664
volition <sean.pgarcia@outlook.com>
2023-09-12 23:00:56 +0800

    added new gui & module init files

---
+++ app/gui/TkinterApp.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+from tkinter import filedialog
+
+import os
+
+
+class TkinterApp():
+    def __init__(self) -> None:
+        self.input_filepath = None
+        self.output_filepath = None
+
+
+    def run_tkinter_app(self):
+        root = tk.Tk()
+        root.geometry("1000x400")
+        root.title("BE(Hons) - Marks Processor")
+        window_frame = tk.Frame(root)
+        window_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+
+
+        # Menu frame
+        menu_frame = tk.LabelFrame(window_frame, text="Menu")
+        menu_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+        menu_frame_button_options = {"pady":5, "padx":5,}
+
+        settings_button = tk.Button(menu_frame, text="Settings", command=root.quit)
+        settings_button.grid(row=0, column=0, sticky='w', **menu_frame_button_options)
+
+        modify_handbook_button = tk.Button(menu_frame, text="Modify Handbook", command=root.quit)
+        modify_handbook_button.grid(row=0, column=1, sticky='w', **menu_frame_button_options)
+
+
+        # File selection frame
+        file_select_frame = tk.LabelFrame(window_frame, text="File Processing")
+        file_select_frame.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
+        file_select_button_width = 10
+        file_select_button_options = {"pady":5, "padx":5, "sticky":'ew'}
+        file_select_label_options = {"pady":5, "padx":5, "sticky":'w'}
+
+        input_file_button = tk.Button(
+            file_select_frame, width=file_select_button_width, text="Input File",
+            command=lambda: self.select_input_file(input_file_label)
+        )
+        input_file_button.grid(row=0, column=0, **file_select_button_options)
+
+        input_file_label = tk.Label(file_select_frame, text="Selected Input File:")
+        input_file_label.grid(row=0, column=1, **file_select_label_options)
+
+        output_file_button = tk.Button(
+            file_select_frame, width=file_select_button_width, text="Output File",
+            command=lambda: self.select_output_file(output_file_label)
+        )
+        output_file_button.grid(row=1, column=0, **file_select_button_options)
+
+        output_file_label = tk.Label(file_select_frame, text="Selected Output File:")
+        output_file_label.grid(row=1, column=1, **file_select_label_options)
+
+
+        # make sure this is grey or not submittable until both input and output variables are specified
+        process_file_button = tk.Button(file_select_frame, width=file_select_button_width, text="Process", command=root.quit)
+        process_file_button.grid(row=2, column=0, **file_select_button_options)
+
+        # Configure row and column weights to make frames expand
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+        window_frame.grid_rowconfigure(1, weight=1)
+        window_frame.grid_columnconfigure(0, weight=1)
+        menu_frame.grid_rowconfigure(1, weight=1)
+        menu_frame.grid_columnconfigure(1, weight=1)
+
+        root.mainloop()
+
+
+    def select_input_file(self, label):
+        truncate_value = 30 # 30 characters
+
+        try:
+            file_path = filedialog.askopenfilename(
+                title="Select a File",
+                filetypes=[("Excel Files", "*.xlsx")]
+            )
+            self.input_filepath = file_path
+            label.config(text=f"Selected Input File: ...{self.input_filepath[-truncate_value:]}")
+
+            return file_path
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return None
+        
+    def select_output_file(self, label):
+        truncate_value = 30 # 30 characters
+
+        try:
+            file_path = filedialog.asksaveasfilename(
+                defaultextension=".xlsx",
+                filetypes=[("Excel Files", "*.xlsx")]
+            )
+            self.output_filepath = file_path
+            label.config(text=f"Selected Output File: ...{self.output_filepath[-truncate_value:]}")
+
+            return file_path
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return None
+
--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-from app.gui import file_selector
+from app.gui.TkinterApp import TkinterApp
 from app.logic import calculate
 import platform
 import subprocess
 
 
 def main():
-    
-    try:
-        import tkinter as tk  # Python 3.x
-    except ImportError:
-        os_type = platform.system()
-        if os_type == 'Darwin':  # Darwin indicates macOS
-            print("You are on macOS. Running the shell command...")
-            subprocess.run("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"", shell=True) # Replace 'ls' with your desired shell command
-            subprocess.run("brew update", shell=True)
-            subprocess.run("brew install python", shell=True)
-            subprocess.run("brew install python-tk", shell=True)          
+    main_window = TkinterApp()
+    main_window.run_tkinter_app()
 
-        else:
-            print(f"You are on {os_type}. The shell command will not be executed.")
-        
-    selected_file_path = file_selector.open_file()
-
-    if selected_file_path:
-        calculate.calculations(selected_file_path)
-    else:
-        print("No file selected.")
 
 if __name__ == "__main__":
     main()
