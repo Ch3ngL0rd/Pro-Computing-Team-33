c693079a13926a139e5ef845d2f6341e29bd66b8
Michael Hodgson <michael.hodgson13@hotmail.com>
2023-09-12 12:05:10 +0800

    check os for tkinter, and try install if needed

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import file_selector
 import calculate
+import platform
+import subprocess
+
+
+
 
 def main():
+    
+    try:
+        import tkinter as tk  # Python 3.x
+    except ImportError:
+        os_type = platform.system()
+        if os_type == 'Darwin':  # Darwin indicates macOS
+            print("You are on macOS. Running the shell command...")
+            subprocess.run("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"", shell=True) # Replace 'ls' with your desired shell command
+            subprocess.run("brew update", shell=True)
+            subprocess.run("brew install python", shell=True)
+            subprocess.run("brew install python-tk", shell=True)          
+
+        else:
+            print(f"You are on {os_type}. The shell command will not be executed.")
+    
+    
+
+
+    
     selected_file_path = file_selector.open_file()
 
     if selected_file_path:
