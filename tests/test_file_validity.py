d3561ce7a7ce8ebf84a96b6f1af403f942cceb00
volition <sean.pgarcia@outlook.com>
2023-09-30 12:52:30 +0800

    fixed imports and set working dir

--- tests/test_file_validity.py
+++ tests/test_file_validity.py
@@ -1,14 +1,24 @@
 import unittest
-import marks_processor
+
+# set path to proj path (parent dir)
+import sys
 import os
+current_dir = os.getcwd()
+proj_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(proj_dir)
+
+# import 
+from src.app.logic.marks_processor import Marks_processor
 
 class TestExcelProcessing(unittest.TestCase):
+    def setUp(self) -> None:
+        self.marks_processor = Marks_processor()
 
     # ================================= Valid file testing =================================
     def test_valid_file_xls(self):
         input_file = 'valid_file.xls'
         
-        result = marks_processor.process_file(input_file)
+        result = self.marks_processor.process_file(input_file)
 
         # Check that the output file was created
         self.assertTrue(os.path.exists(output_file), "Output file should be created")
@@ -16,7 +26,7 @@ class TestExcelProcessing(unittest.TestCase):
     def test_valid_file_xlsx(self):
         input_file = 'valid_file.xlsx'
         
-        result = marks_processor.process_file(input_file)
+        result = self.marks_processor.process_file(input_file)
 
         # Check that the output file was created
         self.assertTrue(os.path.exists(output_file), "Output file should be created")
@@ -30,7 +40,7 @@ class TestExcelProcessing(unittest.TestCase):
 
         # Attempt to process the input file, expecting an exception 
         with self.assertRaises(Exception) as context:
-            marks_processor.process_file(input_file)
+            self.marks_processor.process_file(input_file)
 
         # Define the expected error message
         expected_error_message = "Input file is not an Excel file"
